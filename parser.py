349a917e86c417b84691a976108f534cb64b7b22
esraa.nabil <esraa.nabil@transglobal.com.eg>
2021-09-02 20:03:55 +0200

    xml parsing main functionality

---
+++ parser.py
@@ -0,0 +1,45 @@
+import json
+import os
+import xmltodict
+import time
+
+with open('file1.xml', 'r') as myfile:
+    obj = xmltodict.parse(myfile.read())
+print(json.dumps(obj))
+
+vehicles_list = []
+for key in obj["Insurance"]:
+    for i in obj["Insurance"]["Transaction"]['Customer']['Units']['Auto']['Vehicle']:
+        vehicle = {
+            "id": i['@id'],
+            "make": i['Make'],
+            "vin_number": i['VinNumber'],
+            "model_year": i['ModelYear']
+        }
+
+        vehicles_list.append(vehicle)
+transaction = {
+    "file_name": "xml/file1.xml",
+    "transaction": {
+        "date": obj["Insurance"]["Transaction"]['Date'],
+        "customer": {
+            "id": obj["Insurance"]["Transaction"]['Customer']['@id'],
+            "name": obj["Insurance"]["Transaction"]['Customer']['Name'],
+            "address": obj["Insurance"]["Transaction"]['Customer']['Address'],
+            "phone": obj["Insurance"]["Transaction"]['Customer']['Phone']
+        },
+        "vehicles": vehicles_list
+    }
+}
+print(transaction)
+
+with open('test_file2.json', 'w') as file:
+    json.dump(transaction, file)
+
+cwd = os.getcwd()  # current directory
+os.chdir(cwd)
+timestr = time.strftime("%Y%m%d-%H%M%S")
+print(os.path.basename(__file__))
+
+with open(cwd + "/output/xml" + timestr + "file6.json", "w+") as f:
+    json.dump(transaction, f)
