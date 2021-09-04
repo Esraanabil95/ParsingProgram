f61ab24f51ecb99d811163ae99d853851b9ab450
esraa.nabil <esraa.nabil@transglobal.com.eg>
2021-09-04 16:04:56 +0200

    csv parsing main functionality, add csv in parser

--- parser.py
+++ parser.py
@@ -1,9 +1,13 @@
 from xml_parser import *
+from csv_parser import *
 
 
 def parser(file1, file2=None):
     if file1.endswith('.xml'):
         parsing_xml(file1)
+    elif file1.endswith('.csv') and file2.endswith('.csv'):
+        parsing_csv(file1, file2)
 
 
 parser('file1.xml')
+parser('customers_file1.csv', 'vehicles_file1.csv')
